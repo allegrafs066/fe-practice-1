1a222c4695656538ad92cd60262dca01d93a3ef0
allegras066 <allegrasantoso@gmail.com>
2024-04-08 09:29:52 +0700

    Configured Tailwind and Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="bg-black h-24 relative text-white flex">
+      <div>Logo Container</div>
+      <div>|</div>
+      <div>Help</div>
+
+      <div>Magnifying</div>
+      <div>Menu</div>
+    </div>
+  );
 }
 
 export default Navbar;
